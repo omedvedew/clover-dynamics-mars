37d2b2054d34aace56c31c55a871b3fb2f54391b
olexii <omedvedew@ukr.net>
2021-02-09 01:26:07 +0200

    added cover js

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cover from './Cover/Cover';
+
+const App = () => {
+  return (
+    <>
+      <Cover/>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/App/Cover/Cover.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cover = () => {
+    return (
+        <div className="cover-container">
+            <div className="cover-overlay"></div>
+        </div>
+    )
+}
+
+export default Cover;
