69f76417d646465772d553cb3abd7d661226c964
olexii <omedvedew@ukr.net>
2021-02-09 21:15:55 +0200

    removed photos templates

--- src/App/Main/PhotoItems/PhotoItems.js
+++ src/App/Main/PhotoItems/PhotoItems.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import Photo from './Photo';
-import photos from './photos';
-
 const PhotoItems = () => {
     return (
         <div className="photo-items-container">
