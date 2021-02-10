03ed14b03f9f8a312634ff3193de8e52186b962f
olexii <omedvedew@ukr.net>
2021-02-10 02:51:34 +0200

    modified logo link

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <a className="logo row" href="/">
+        <a className="logo row" href="">
             <div className="logo-img"></div>
             <div className="logo-title">CLOVER DYNAMICS</div>
         </a>
